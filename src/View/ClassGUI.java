421b1dd3302983a72003223144722e697de8a818
binhnt3002 <tanbinh159@gmail.com>
2024-12-29 19:55:22 +0700

    initalize project

---
+++ src/View/ClassGUI.java
@@ -0,0 +1,25 @@
+package View;
+
+import javax.swing.*;
+
+public class ClassGUI extends JPanel implements InterfaceGUI  {
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void displaySuccess() {
+
+    }
+
+    @Override
+    public void displayError() {
+
+    }
+
+    @Override
+    public boolean checkInternetConnection(String url) {
+        return false;
+    }
+}
---
+++ src/View/InterfaceGUI.java
@@ -0,0 +1,10 @@
+package View;
+
+public interface InterfaceGUI {
+    public void display();
+
+    public void displaySuccess();
+    public void displayError();
+    public boolean checkInternetConnection(String url);
+
+}
